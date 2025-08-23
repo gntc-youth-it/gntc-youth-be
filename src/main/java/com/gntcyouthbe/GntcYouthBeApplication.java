f247e6617bf4ea638482729ca57ae5fafd0dbf65
StoneHee99 <kiit0901@gmail.com>
2025-08-23 23:00:54 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/gntcyouthbe/GntcYouthBeApplication.java
@@ -0,0 +1,13 @@
+package com.gntcyouthbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GntcYouthBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GntcYouthBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gntcyouthbe/GntcYouthBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gntcyouthbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GntcYouthBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
